6764795ac57817ba4bdafe72557760b30523c4d1
MatttTang <46803888+MatttTang@users.noreply.github.com>
2021-01-05 17:38:37 -0800

    Adding return to secret move

--- src/Modules/SecretMove.js
+++ src/Modules/SecretMove.js
@@ -2,15 +2,27 @@ import React from 'react'
 import Video from '../assets/vid.mp4';
 import '../styler/video.css';
 import {motion} from 'framer-motion';
+import {Link} from 'react-router-dom';
+import PV from './pageVariants';
 
 export default function SecretMove() {
     return (
-        <div>
-            <motion.video className="vid" autoPlay loop
+        <motion.div style={{textAlign: 'center'}}
+            initial="initial"
+            animate="in"
+            exit="out"
+            variants={PV}>
+            <motion.video id="video" className="vid" autoPlay loop
                 initial={{y: -1000, x: -1000}}
                 animate={{y: 0, x: -200}}>
                 <source src={Video} />
             </motion.video>
-        </div>
+
+            <Link to="/">
+                <button className="mainButtons">
+                    Return
+                </button>
+            </Link>
+        </motion.div>
     )
 }
